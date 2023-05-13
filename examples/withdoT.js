7cd5e0455a179c348103edcbb65235591ecc8f55
Zandario <zandarioh@gmail.com>
2023-05-12 20:41:03 -0500

    v2-eslinted

--- examples/withdoT.js
+++ examples/withdoT.js
@@ -1,5 +1,5 @@
 (function() {
-	var doT = require('../doT.js'),
+	const doT = require('../doT.js'),
 		fs = require('fs'),
 		data = { name: "Foo", f1: 1, f2: 2, f3: 3, altEmail: "conditional works", farray:[{farray:[1,2,3,[11,22,33]],person:{name:'Ell',age:23}},{farray:{how:'really'}}, {farray:[5,6,7,8]}]},
 		defs = { a: 100, b: 200};
@@ -9,11 +9,11 @@
 	};
 	defs.externalsnippet = defs.loadfile('/snippet.txt');
 
-	fs.readFile(process.argv[1].replace(/\/[^\/]*$/,'/advancedsnippet.txt'), function (err, snippet) {
+	fs.readFile(process.argv[1].replace(/\/[^\/]*$/,'/advancedsnippet.txt'), (err, snippet) => {
 		if (err) {
 			console.log("Error reading advancedsnippet.txt " + err);
 		} else {
-			var doTCompiled = doT.template(snippet.toString(), undefined, defs);
+			const doTCompiled = doT.template(snippet.toString(), undefined, defs);
 			console.log("Generated function: \n" + doTCompiled.toString());
 			console.log("Result of calling with " + JSON.stringify(data) + " :\n" + doTCompiled(data));
 		}
